78341d5c2bd9a698bb8c2269aa0025836a04daaf
Vitaliy <103601190+Yevchu@users.noreply.github.com>
2023-09-24 18:16:48 +0300

    Add files via upload

---
+++ rest_app/src/schemas.py
@@ -0,0 +1,46 @@
+from datetime import datetime, date
+from pydantic import BaseModel, Field, EmailStr
+
+class ResponseModel(BaseModel):
+    id: int
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    email: EmailStr = Field(max_length=120)
+    birthday: date
+    description: str | None = None
+
+    class Config:
+        from_attributes = True
+
+class ContactModel(BaseModel):
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    email: EmailStr = Field(max_length=120)
+    birthday: date
+    description: str | None = None
+
+    class Config:
+        from_attributes = True
+
+class UserModel(BaseModel): 
+    username: str = Field(min_length=5, max_length=16)
+    email: str
+    password: str = Field(min_length=5, max_length=16)
+
+class UserBb(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class UserResponse(BaseModel):
+    user: UserBb
+    detail: str = 'User successfully created'
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = 'bearer'
